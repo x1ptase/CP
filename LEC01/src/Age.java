fa2d5b596d0e6c60688294f659274e7691a796c7
x1ptadkm <x1ptadkm@gmail.com>
2025-06-04 21:36:39 +0700

    update src

---
+++ LEC01/src/Age.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+        
+public class Age {
+    public static void main(String[] args){
+        Scanner inp=new Scanner(System.in);
+        int age=inp.nextInt();
+        System.out.println(2019 - age);
+    }
+}
