fdc05d9302b4524631a8e3da2e6411cd6bdf68d9
Anh Pham Tuan <x1ptadkm@gmail.com>
2025-06-28 15:41:23 +0700

    update src

---
+++ LEC12-LinkedList/LEC12/src/MinimumElement.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+/*
+Determine the smallest number in an integer linked list
+INPUT:
+    Including several lines, each contains an integer <= 10^6
+    0 is the signal terminating the linked list (0 IS NOT INCLUDING IN THE LIST)
+    Data ensures that there are at least 1 element in the list. The number of element <= 10^5.
+OUTPUT:
+    A single line is the smallest number in the linked list
+TC1:
+input
+2
+3
+0
+output
+2
+*/
+
+
+class Node {
+    int info;
+    Node next;
+    
+    public Node() {
+        this.info = 0;
+        this.next = null;
+    }
+    
+    public Node(int info, Node next) {
+        this.info = info;
+        this.next = next;
+    }
+}
+
+class MyList {
+    Node head, tail;
+    int size;
+    
+    public MyList() {
+        head = tail = null;
+        size = 0;
+    }
+    
+    public boolean isEmpty() {
+        return head == null;
+    }
+    
+    public void clear() {
+        head = tail = null;
+        size = 0;
+    }
+    
+    public void addLast(int x) {
+        Node p = new Node(x, null);
+        if (isEmpty()) {
+            head = tail = p;
+        } else {
+            tail.next = p;
+            tail = p;
+        }
+        size++;
+    }
+    
+    public int minNode() {
+        if (isEmpty()) {
+            return Integer.MAX_VALUE; // Trường hợp danh sách rỗng
+        }
+        int min = head.info;
+        Node curr = head;
+        while (curr != null) {
+            if (curr.info < min) {
+                min = curr.info;
+            }
+            curr = curr.next;
+        }
+        return min;
+    }
+}
+
+public class MinimumElement {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        MyList list = new MyList();
+        
+        // Nhập danh sách cho đến khi gặp số 0
+        while (true) {
+            int x = inp.nextInt();
+            if (x == 0) {
+                break; // Thoát khi gặp số 0
+            }
+            list.addLast(x);
+        }
+        
+        // In ra số nhỏ nhất
+        System.out.println(list.minNode());
+        
+        inp.close();
+    }
+}
